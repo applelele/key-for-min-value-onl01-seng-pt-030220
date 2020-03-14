7d3bd6c65461bd3ac8de0ab4db1db2e6da24921b
YUKIKO IKEDA <applelele@gmail.com>
2020-03-14 02:26:50 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,28 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
 
+def key_for_min_value(name_hash)
+  smallest_name = nil
+  smallest_num = nil
+  name_hash.each do |name, num|
+  if smallest_num == nil || num < smallest_num
+    smallest_num = num
+    smallest_name = name    
+  end
+  end
+  return smallest_name
 end
+
+# def key_for_min_value(name_hash)
+#   smallest_name = nil
+#   smallest_num = nil
+#   name_hash.each do |name, num|
+#   if num < smallest_num
+#     smallest_num = num
+#     smallest_name = name
+#   elsif name_hash.size == 0
+#     smallest_name == nil
+#   end
+#     return smallest_name
+#   end
