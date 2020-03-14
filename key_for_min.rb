ed7110eeaf446f9e5993fc1b35d1211621d4e856
YUKIKO IKEDA <applelele@gmail.com>
2020-03-14 02:05:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,15 +2,28 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 
+# def key_for_min_value(name_hash)
+#   smallest_name = nil
+#   smallest_num = nil
+#   name_hash.each do |name, num|
+#   if smallest_num == nil || num < smallest_num
+#     smallest_num = num
+#     smallest_name = name    
+#   end
+#     return smallest_name
+#   end
+# end
+
 def key_for_min_value(name_hash)
   smallest_name = nil
   smallest_num = nil
   name_hash.each do |name, num|
-  if smallest_num == nil || num < smallest_num
+  if num < smallest_num
     smallest_num = num
-    smallest_name = name    
+    smallest_name = name
+  elsif name_hash.size == 0
+    smallest_num == nil
   end
     return smallest_name
   end
 end
-
